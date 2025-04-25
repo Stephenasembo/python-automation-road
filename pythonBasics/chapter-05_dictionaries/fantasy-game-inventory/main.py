e16ca0484a9b5d8cf99cfb981d79960beb9737b7
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2025-04-25 22:54:56 +0300

    Feat(C05): Add Items To The Inventory

--- pythonBasics/chapter-05_dictionaries/fantasy-game-inventory/main.py
+++ pythonBasics/chapter-05_dictionaries/fantasy-game-inventory/main.py
@@ -1,5 +1,3 @@
-import pprint
-
 def displayInventory(inventory):
   totalItems = 0
   print('Inventory:')
@@ -8,6 +6,17 @@ def displayInventory(inventory):
     print(str(value) + ' ' + str(key))
   print('Total number of items: ' + str(totalItems))
 
-example = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
+def addToInventory(inventory, addedItems):
+  for listValue in addedItems:
+    inventory.setdefault(listValue, 0)
+    inventory[listValue] += 1
+  return inventory
 
-displayInventory(example)
+inventoryExample = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
+
+# Example of a list to be added to the inventory
+dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby', 'knife']
+
+inventoryExample = addToInventory(inventoryExample, dragonLoot)
+
+displayInventory(inventoryExample)
