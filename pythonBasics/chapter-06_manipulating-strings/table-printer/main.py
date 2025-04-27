718e0070637c9aa5a6d1de5209a6c272a26078ec
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2025-04-27 12:09:16 +0300

    Feat(C06): Find Longest String In Each Inner List
    The longest string will be the width of the column

---
+++ pythonBasics/chapter-06_manipulating-strings/table-printer/main.py
@@ -0,0 +1,17 @@
+#! /usr/bin/python3
+
+sample = [['apples', 'oranges', 'cherries', 'banana'],
+          ['Alice', 'Bob', 'Carol', 'David'],
+          ['dogs', 'cats', 'moose', 'goose']]
+
+def printTable(tableData):
+  # Find longest string for each column
+  colWidths = [0] * len(tableData)
+  for i in range(len(tableData)):
+    longestStr = len(tableData[i][0])
+    for value in range(len(tableData[i]) - 1):
+      if longestStr < len(tableData[i][value + 1]):
+        longestStr = len(tableData[i][value + 1])
+    colWidths[i] = longestStr
+
+printTable(sample)
