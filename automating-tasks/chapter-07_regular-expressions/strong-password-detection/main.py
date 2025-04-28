7df182ebe7a4f1c6834bb72c1b8e58c5f2f5c2e1
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2025-04-28 21:11:14 +0300

    Refactor(C07): Clean Up Control Flow Logic
    Reducing the amount of code makes the code easier to read

--- automating-tasks/chapter-07_regular-expressions/strong-password-detection/main.py
+++ automating-tasks/chapter-07_regular-expressions/strong-password-detection/main.py
@@ -1,52 +1,25 @@
 #! /usr/bin/python3
 
-import re, sys
+import re
 
-# Regex to make sure the password is atleast 8 characters long
-passwordLenRegex = re.compile(r'(\w){8,}')
+userPassword = 'stephe7nNnnn'
 
-digitRegex = re.compile(r'\d')
+def isPasswordStrong():
+  # Regexes check if password is strong
+  passwordLenRegex = re.compile(r'(\w){8,}')
+  digitRegex = re.compile(r'\d')
+  lowerCaseRegex = re.compile(r'[a-z]')
+  upperCaseRegex = re.compile(r'[A-Z]')
 
-lowerCaseRegex = re.compile(r'[a-z]')
+  if passwordLenRegex.search(userPassword) == None:
+    return 'The password is weak. The password should have at least 8 characters.'
+  if lowerCaseRegex.search(userPassword) == None:
+    return 'The password is weak. The password should have at least 1 lowercase character.'
+  if upperCaseRegex.search(userPassword) == None:
+    return 'The password is weak. The password should have at least 1 uppercase character.'
+  if digitRegex.search(userPassword) == None:
+    return 'The password is weak. The password should have at least 1 digit.'
+  
+  return 'The password is strong.'
 
-upperCaseRegex = re.compile(r'[A-Z]')
-
-userPassword = 'stephenmArk22'
-
-lenMatch = passwordLenRegex.search(userPassword)
-
-if lenMatch == None:
-  print('''
-  The password is weak.
-  Please run the program again with a stronger password.
-  The password should have at least 8 characters.
-  ''')
-
-else:
-  lowerCaseMatch = lowerCaseRegex.search(userPassword)
-  if lowerCaseMatch == None:
-    print('''
-    The password is weak.
-    Please run the program again with a stronger password.
-    The password should have at least 1 lowercase character.
-    ''')
-
-  else:
-    upperCaseMatch = upperCaseRegex.search(userPassword)
-    if upperCaseMatch == None:
-      print('''
-      The password is weak.
-      Please run the program again with a stronger password.
-      The password should have at least 1 uppercase character.
-      ''')
-
-    else:
-      digitMatch = digitRegex.search(userPassword)
-      if digitMatch == None:
-        print('''
-        The password is weak.
-        Please run the program again with a stronger password.
-        The password should have at least 1 digit.
-        ''')
-      else:
-          print('The password entered is strong.')
+print(isPasswordStrong())
