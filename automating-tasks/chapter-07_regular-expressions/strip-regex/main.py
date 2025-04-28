7e921c53e77403956214c50cc5d7444189163d92
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2025-04-28 21:48:28 +0300

    Fix(C07): Handle Only 1 Command Line Argument
    The program was crashing if only one command line argument
    was being provided

--- automating-tasks/chapter-07_regular-expressions/strip-regex/main.py
+++ automating-tasks/chapter-07_regular-expressions/strip-regex/main.py
@@ -15,4 +15,7 @@ if len(sys.argv) > 3 or len(sys.argv) < 2:
   print('Usage: python string character.')
   sys.exit()
 else:
-  print(stripRegex(sys.argv[1], sys.argv[2]))
+  if len(sys.argv) == 2:
+    print(stripRegex(sys.argv[1]))
+  else:
+    print(stripRegex(sys.argv[1], sys.argv[2]))
