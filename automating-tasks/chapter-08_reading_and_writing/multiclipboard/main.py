3d9eb705a864ea9d07ae5ccc6d41043e9b54be0d
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2025-05-01 19:12:21 +0300

    Feat(C08): Implement Delete Feature To The Program

--- automating-tasks/chapter-08_reading_and_writing/multiclipboard/main.py
+++ automating-tasks/chapter-08_reading_and_writing/multiclipboard/main.py
@@ -4,20 +4,29 @@
 
 '''Usage: python save <keyword> - Saves clipboard to keyword.
           python <keyword> - Loads keyword to clipboard.
-          python list - Loads all keywords to clipboard.'''
+          python list - Loads all keywords to clipboard.
+          python delete <keyword> - Deletes keyword from storage.
+          python delete - Deletes all keywords from storage.'''
         
 import shelve, pyperclip, sys
 
 mcbShelf = shelve.open('mcb')
 
 # TODO: Save clipboard content.
-if len(sys.argv) == 3 and sys.argv[1].lower() == 'save':
-  mcbShelf[sys.argv[2]] = pyperclip.paste()
+if len(sys.argv) == 3:
+  if sys.argv[1].lower() == 'save':
+    mcbShelf[sys.argv[2]] = pyperclip.paste()
+  elif sys.argv[1].lower() == 'delete' and sys.argv[2] in mcbShelf:
+    del mcbShelf[sys.argv[2]]
+
 elif len(sys.argv) == 2:
   # TODO: List keywords and load content.
   if sys.argv[1].lower() == 'list':
     pyperclip.copy(str(list(mcbShelf.keys())))
   elif sys.argv[1] in mcbShelf:
     pyperclip.copy(mcbShelf[sys.argv[1]])
+  elif sys.argv[1].lower() == 'delete':
+    for key in list(mcbShelf.keys()):
+      del mcbShelf[key]
 
 mcbShelf.close()
