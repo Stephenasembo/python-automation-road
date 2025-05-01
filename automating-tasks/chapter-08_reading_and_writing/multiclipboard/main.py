f4a17d79352614c004a647add113bfb5a6f6ac19
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2025-05-01 18:42:28 +0300

    Feat(C08): Create Initial Program
    The task is to extend the functionality of the program

---
+++ automating-tasks/chapter-08_reading_and_writing/multiclipboard/main.py
@@ -0,0 +1,23 @@
+#! /usr/bin/python3
+
+# mcb.pyw - Saves and loads data to and from a clipboard
+
+'''Usage: python save <keyword> - Saves clipboard to keyword.
+          python <keyword> - Loads keyword to clipboard.
+          python list - Loads all keywords to clipboard.'''
+        
+import shelve, pyperclip, sys
+
+mcbShelf = shelve.open('mcb')
+
+# TODO: Save clipboard content.
+if len(sys.argv) == 3 and sys.argv[1].lower() == 'save':
+  mcbShelf[sys.argv[2]] = pyperclip.paste()
+elif len(sys.argv) == 2:
+  # TODO: List keywords and load content.
+  if sys.argv[1].lower() == 'list':
+    pyperclip.copy(str(list(mcbShelf.keys())))
+  elif sys.argv[1] in mcbShelf:
+    pyperclip.copy(mcbShelf[sys.argv[1]])
+
+mcbShelf.close()
