55fb4767077f846748850c2bc402cb94debf6352
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2025-05-02 22:01:17 +0300

    Feat(C08): Create Project Skeleton
    It acts as a guide for program development

---
+++ automating-tasks/chapter-08_reading_and_writing/mad-libs/main.py
@@ -0,0 +1,15 @@
+#! /usr/bin/python3
+
+# mad-libs -Replace content in text file with user input
+
+''' User enters words which will replace specific word content in the text file.
+    String result is printed on the screen and saved to a new text file.'''
+
+# TODO: Open text file
+# TODO: Read file content
+# TODO: Prompt user for input
+# TODO: Perform word search with regex expressions
+# TODO: Create new file for results
+# TODO: Substitute matches with corresponding input
+# TODO: Write the result to the new file
+# TODO: Close open files
