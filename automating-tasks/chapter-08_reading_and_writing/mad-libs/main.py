83f74b62690a59fd80e8612dfd5b21d5e584db9d
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2025-05-02 22:56:17 +0300

    Feat(C08): Implement Project Solution

--- automating-tasks/chapter-08_reading_and_writing/mad-libs/main.py
+++ automating-tasks/chapter-08_reading_and_writing/mad-libs/main.py
@@ -5,11 +5,38 @@
 ''' User enters words which will replace specific word content in the text file.
     String result is printed on the screen and saved to a new text file.'''
 
+import re
+
 # TODO: Open text file
+initialFile = open('./sampleText.txt')
+
 # TODO: Read file content
+initialFileContent = initialFile.read()
+
 # TODO: Prompt user for input
+adjective = input('Enter an adjective:\n')
+noun1 = input('Enter a noun:\n')
+verb = input('Enter a verb:\n')
+noun2 = input('Enter a noun:\n')
+
 # TODO: Perform word search with regex expressions
-# TODO: Create new file for results
+adjectiveRegex = re.compile('ADJECTIVE')
+nounRegex = re.compile('NOUN')
+verbRegex = re.compile('VERB')
+
 # TODO: Substitute matches with corresponding input
+newContent = adjectiveRegex.sub(adjective, initialFileContent)
+newContent = nounRegex.sub(noun1, newContent, count=1)
+newContent = verbRegex.sub(verb, newContent)
+newContent = nounRegex.sub(noun2, newContent)
+
+# TODO: Create new file for results
+resultFile = open('./result.txt', 'w')
+
 # TODO: Write the result to the new file
+print(newContent)
+resultFile.write(newContent)
+
 # TODO: Close open files
+initialFile.close()
+resultFile.close()
