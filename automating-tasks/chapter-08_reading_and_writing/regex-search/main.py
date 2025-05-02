0752f2ed44e2065b9bcd724cb5fcb54f4fbec2eb
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2025-05-02 23:08:57 +0300

    Feat(C08): Improve Code Understability
    The comments explain the functionality and guide to developing
    the program

---
+++ automating-tasks/chapter-08_reading_and_writing/regex-search/main.py
@@ -0,0 +1,14 @@
+#! /usr/bin/python3
+
+# regex-search - Search for line matching a user-supplied regex expression
+
+''' Search for any line in all .txt files that matches the user supplied
+    regex expression. Print the results to the screen.
+'''
+
+import re
+
+# TODO: Receive regex expression from user as input
+# TODO: Open all .txt files in a folder
+# TODO: Search for a line match
+# TODO: Print the line to the screen
