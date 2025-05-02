63175669e109550b1a5ce0c57a47449bd575c0b8
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2025-05-02 23:54:12 +0300

    Feat(C08): Implement Program Functionality

--- automating-tasks/chapter-08_reading_and_writing/regex-search/main.py
+++ automating-tasks/chapter-08_reading_and_writing/regex-search/main.py
@@ -6,9 +6,25 @@
     regex expression. Print the results to the screen.
 '''
 
-import re
+import re, os
 
 # TODO: Receive regex expression from user as input
+userRegex = re.compile(input('Enter the regex expression:\n'))
+
 # TODO: Open all .txt files in a folder
+contentArray = []
+for fileName in os.listdir('./sampleFolder'):
+    # Open only .txt files
+    if fileName.endswith('.txt'):
+        file = open(os.path.join('./sampleFolder', fileName))
+        fileContent = file.readlines()
+        contentArray += fileContent
+        file.close()
+
 # TODO: Search for a line match
-# TODO: Print the line to the screen
+for line in contentArray:
+    matchObject = userRegex.search(line)
+
+    # TODO: Print the line to the screen
+    if matchObject != None:
+        print(line)
