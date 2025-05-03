9cfe2bbcbb5ee5ee0df7c8ce24780aa677d519e6
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2025-05-03 23:16:56 +0300

    Feat(C09): Implement Folder Traversal
    This is essential for searching for the specified type of files

--- automating-tasks/chapter-09_organizing_files/selective_copy/main.py
+++ automating-tasks/chapter-09_organizing_files/selective_copy/main.py
@@ -3,8 +3,20 @@
 '''Selective copy - Searches for files with certain file extensions.
     The files then get copied to a new folder.'''
 
+import os, re, shutil
+
 # TODO: Get file extensions for files to be searched.
 
+# extension = input('Which type of files do you want to copy ?\n')
+# Testing purposes
+extension = 'pdf'
+
 # TODO: Walk through the folder searching for the files.
+for folder, subFolders, files in os.walk('.'):
+  print(folder)
+  for subfolder in subFolders:
+    print(subfolder)
+  for fileName in files:
+    print(fileName)
 
 # TODO: Copy found files to a new folder.
