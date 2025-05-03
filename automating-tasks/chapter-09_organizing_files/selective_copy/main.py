abfdd41d81dfb166f7b8ff103fecc4c38b876e6b
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2025-05-04 00:14:16 +0300

    Feat(C09): Copy Found Files To A New Folder
    The task for the project assignment is complete

--- automating-tasks/chapter-09_organizing_files/selective_copy/main.py
+++ automating-tasks/chapter-09_organizing_files/selective_copy/main.py
@@ -7,16 +7,23 @@ import os, re, shutil
 
 # TODO: Get file extensions for files to be searched.
 
-# extension = input('Which type of files do you want to copy ?\n')
-# Testing purposes
-extension = '.pdf'
+extension = input('Which type of files do you want to copy ?\n')
 extensionRegex = re.compile(extension)
 
 # TODO: Walk through the folder searching for the files.
+foundFiles = []
 for folder, subFolders, files in os.walk('.'):
+  name = os.path.join(os.getcwd(), folder)  
   for fileName in files:
     fileMatch = extensionRegex.search(fileName)
     if fileMatch != None:
-      print('Found your file: ' + fileName)
+      filePath = os.path.join(name, fileName)
+      foundFiles.append(filePath)
 
 # TODO: Copy found files to a new folder.
+folderLocation = os.path.join(os.getcwd(), 'foundFiles')
+if not (os.path.isdir(folderLocation)):
+  os.makedirs(folderLocation)
+  folderLocation = os.path.join(os.getcwd(), 'foundFiles')
+for file in foundFiles:
+  shutil.copy(file, folderLocation)
