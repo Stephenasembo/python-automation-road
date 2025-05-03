e82f6369ce6831cb4a7bf05f1650fe1923b23fa6
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2025-05-03 23:09:43 +0300

    Feat(C09): Improve Code Understandability
    The comments provide information on the functionality of the
    program and the todos act as guides for development.

---
+++ automating-tasks/chapter-09_organizing_files/selective_copy/main.py
@@ -0,0 +1,10 @@
+#! /usr/bin/python3
+
+'''Selective copy - Searches for files with certain file extensions.
+    The files then get copied to a new folder.'''
+
+# TODO: Get file extensions for files to be searched.
+
+# TODO: Walk through the folder searching for the files.
+
+# TODO: Copy found files to a new folder.
