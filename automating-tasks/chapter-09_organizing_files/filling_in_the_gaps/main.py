fd9084b97ba107de8007460823121dea6aba15bd
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2025-05-04 12:31:14 +0300

    Feat(C09): Improve Code Understandability
    The comments provide information on the program's functionality and
    serves as a guide for development

---
+++ automating-tasks/chapter-09_organizing_files/filling_in_the_gaps/main.py
@@ -0,0 +1,18 @@
+#! /usr/bin/python3
+
+''' Filling in the gaps - Searches for gaps in file naming in a specified folder.
+    If gaps are found, the program renames the file and all proceeding files to close
+    the gap between the filenames.
+'''
+
+import os, re
+
+# TODO: Walk through folder
+
+# TODO: Find files with specified prefix
+
+# TODO: Store all found files in a list
+
+# TODO: Look for gaps in file naming
+
+# TODO: If gap found rename the file and files after it
