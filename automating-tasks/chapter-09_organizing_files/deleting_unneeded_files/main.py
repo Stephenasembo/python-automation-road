102ea61a8c839f4695d2d48f98288041ecec82b8
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2025-05-04 10:59:09 +0300

    Feat(C09): Improve Code Understandability
    The comments provide information on program functionality and serve as
    a guide for development

---
+++ automating-tasks/chapter-09_organizing_files/deleting_unneeded_files/main.py
@@ -0,0 +1,14 @@
+#! /usr/bin/python3
+
+'''Deleting unneeded files - Walks through the entire folder
+      tree looking for exceptionally large files.
+      The large files' absolute paths are then printed out to the screen.
+'''
+
+import os
+
+# TODO: Walk entire folder tree provided.
+
+# TODO: Get file size of each file during the walk.
+
+# TODO: Store large file names in a list and print out the list.
