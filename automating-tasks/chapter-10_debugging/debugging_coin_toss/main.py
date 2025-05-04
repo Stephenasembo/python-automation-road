dc97c1f0d1497c1a0114048e067910bcdf5148ef
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2025-05-04 22:02:26 +0300

    Feat(C10): Use Logging Messages To Debug
    Using the logging module is easier for debugging than print functions

--- automating-tasks/chapter-10_debugging/debugging_coin_toss/main.py
+++ automating-tasks/chapter-10_debugging/debugging_coin_toss/main.py
@@ -5,18 +5,35 @@ Debugging coin toss - The task is to run the program,
 find and fix bugs through debugging.
 '''
 
-import random
+import random, logging
+
+logging.basicConfig(filename = 'logs.txt', level = logging.DEBUG,
+  format='%(asctime)s - %(levelname)s - %(message)s')
+
+logging.debug('Start of program')
 guess = ''
 while guess not in ('heads', 'tails'):
   print('Guess the coin toss! Enter heads or tails:')
   guess = input()
+
+logging.debug('The user\'s choice is: ' + guess)
 toss = random.randint(0, 1) # 0 is tails, 1 is heads
+logging.debug('The coin toss is currently: ' + str(toss))
+
 if toss == guess:
   print('You got it!')
+  logging.debug('End of program')
+
 else:
   print('Nope! Guess again!')
+  logging.debug('An extra attempt awarded to player.')
   guesss = input()
+  logging.debug('The second guess from the player is: ' + guess)
+
   if toss == guess:
     print('You got it!')
+    logging.debug('End of program')
+
   else:
     print('Nope. You are really bad at this game.')
+    logging.debug('End of program')
