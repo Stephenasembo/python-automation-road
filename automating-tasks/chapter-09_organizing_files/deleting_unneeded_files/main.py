e7089fa9d61050ac337555a805623ad64edf4ea5
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2025-05-04 11:38:09 +0300

    Feat(C09): Implement Prototype As Solution
    The program will be refactored to work better later on

--- automating-tasks/chapter-09_organizing_files/deleting_unneeded_files/main.py
+++ automating-tasks/chapter-09_organizing_files/deleting_unneeded_files/main.py
@@ -7,8 +7,24 @@
 
 import os
 
-# TODO: Walk entire folder tree provided.
+# Test folder will be the home folder.
+folder = '/home/sain8op/'
+maxSize = 1000000
+largeFiles = []
 
-# TODO: Get file size of each file during the walk.
+# TODO: Walk entire folder tree provided.
+for folderName, subfolderNames, fileNames in os.walk(folder):
+  # TODO: Get file size of each file during the walk.
+  for file in fileNames:
+    filePath = os.path.join(folderName, file)
+    if os.path.isfile(filePath):
+      fileSize = os.path.getsize(filePath)
+      if fileSize > maxSize:
+        largeFiles.append(filePath)
 
 # TODO: Store large file names in a list and print out the list.
+if len(largeFiles) != 0:
+  for largeFile in largeFiles:
+    print(largeFile)
+else:
+  print('No file larger than ' + str(maxSize) + ' found.')
