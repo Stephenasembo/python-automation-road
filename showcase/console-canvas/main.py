378addcc3d71e5241457e066099cc1d7e53d0125
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2025-05-09 19:35:22 +0300

    Chore(S01): Explain Program Functionality
    The comments explain what the program does and acts as a development
    roadmap

---
+++ showcase/console-canvas/main.py
@@ -0,0 +1,8 @@
+#! /usr/bin/python3
+
+# Console Canvas - Prints the user desired shape to the terminal.
+
+# TODO: Inform user of possible options for shape
+# TODO: Ask user for desired shape
+# TODO: Ask user for shape's size
+# TODO: Print the shape to the console
