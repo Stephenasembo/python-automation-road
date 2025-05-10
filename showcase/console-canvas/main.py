61ee0dea5411422c949b16a482c078d0524c8da6
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2025-05-10 12:12:02 +0300

    Feat(S01): Create Shape Printing Functions

--- showcase/console-canvas/main.py
+++ showcase/console-canvas/main.py
@@ -15,5 +15,28 @@ user_shape = input('Which shape do you want to print?\n')
 
 # TODO: Ask user for shape's size
 user_size = input('Which size do you want your ' + user_shape + ' to be?\n')
+user_size = int(user_size)
 
 # TODO: Print the shape to the console
+symbol = '#'
+
+def print_square():
+  for i in range(user_size):
+    for j in range(user_size):
+      print(symbol, end='')
+    print()
+
+def print_triangle():
+  for i in range(1, user_size + 1):
+    for j in range(i):
+      print(symbol, end='')
+    print()
+
+def print_pyramid():
+  padding = user_size - 1
+  space = ' '
+  pyramid_symbol = symbol
+  for row in range(user_size):
+    print((space * padding) + pyramid_symbol)
+    pyramid_symbol += symbol * 2
+    padding -= 1
