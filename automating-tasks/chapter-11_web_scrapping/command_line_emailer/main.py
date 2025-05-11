5a56d240cac5a50e2c8c51fbb63079ac55600bf1
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2025-05-11 22:27:21 +0300

    Feat(C11): Get User Email Account Details
    These details will be used to login to the user's email account in order
    to send the email to the recipient

--- automating-tasks/chapter-11_web_scrapping/command_line_emailer/main.py
+++ automating-tasks/chapter-11_web_scrapping/command_line_emailer/main.py
@@ -16,10 +16,31 @@ else:
   recipient = sys.argv[1]
   message = sys.argv[2:]
 
-logging.info('Program reached end of execution.')
-
 # TODO: Ask for user's account details
+print('\nEnter your account details to log in to you email account.\n')
+
+while True:
+  try:
+    email_account = input('Email address: ')
+    if email_account == '':
+      raise Exception('\nError: Email address can not be empty! Please try again.\n')
+    else:
+      break
+  except Exception as err:
+    print(err)
+
+while True:
+  try:
+    email_password = input('Password: ')
+    if email_password == '':
+      raise Exception('\nError: Password can not be empty! Please try again.\n')
+    else:
+      break
+  except Exception as err:
+    print(err)
 
 # TODO: Send email to specified address
 
 # TODO: Exit program
+sys.exit()
+logging.info('Program reached end of execution.')
