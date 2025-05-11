f2f784b176549f82d5f19058cd161c1aadc223b2
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2025-05-11 21:57:55 +0300

    Chore(C11): Provide Program Functionality Details
    The comments explain the purpose of the program and serve as a
    development roadmap

---
+++ automating-tasks/chapter-11_web_scrapping/command_line_emailer/main.py
@@ -0,0 +1,13 @@
+#! /usr/bin/python3
+
+# Command line emailer - Sends an email from the command line
+
+import sys
+
+# TODO: Take email address and email messsage from command line
+
+# TODO: Ask for user's account details
+
+# TODO: Send email to specified address
+
+# TODO: Exit program
