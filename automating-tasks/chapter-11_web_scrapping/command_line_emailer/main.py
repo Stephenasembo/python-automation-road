85ea13edd6522a981022c61976c3dfb2b447865c
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2025-05-11 22:14:01 +0300

    Feat(C11): Receive Email Message And Recipient
    The recipient and the email message are obtained from the command line

--- automating-tasks/chapter-11_web_scrapping/command_line_emailer/main.py
+++ automating-tasks/chapter-11_web_scrapping/command_line_emailer/main.py
@@ -2,9 +2,21 @@
 
 # Command line emailer - Sends an email from the command line
 
-import sys
+import sys, logging
+
+logging.basicConfig(level=logging.DEBUG, filename='./logs.txt', format='%(asctime)s - %(levelname)s - %(message)s')
 
 # TODO: Take email address and email messsage from command line
+logging.info('Program started.')
+if len(sys.argv) < 3:
+  print('Error: python recipient-email-address message\n')
+  logging.error('Program ended not enough command line arguments provided.')
+  sys.exit()
+else:
+  recipient = sys.argv[1]
+  message = sys.argv[2:]
+
+logging.info('Program reached end of execution.')
 
 # TODO: Ask for user's account details
 
