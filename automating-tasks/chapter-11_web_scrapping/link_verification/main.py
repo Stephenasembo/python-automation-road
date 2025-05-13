5b3cf3954077e40364305854efcc4baaf9c3c350
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2025-05-13 09:56:54 +0300

    Feat(C11): Open Initial Webpage
    The initial webpage is from the url provided by the user

--- automating-tasks/chapter-11_web_scrapping/link_verification/main.py
+++ automating-tasks/chapter-11_web_scrapping/link_verification/main.py
@@ -2,7 +2,18 @@
 
 # Link_verification - Flags broken links from a provided url to a webpage
 
+import requests, sys, bs4
+
+# Test website
+url = 'https://python.org'
+
 # TODO: Open webpage from the provided url
+try:
+  initial_page = requests.get(url)
+  initial_page.raise_for_status()
+except Exception as err:
+  print(f'There was a problem loading: {url}')
+  sys.exit()
 
 # TODO: Get all link elements from the webpage
 
