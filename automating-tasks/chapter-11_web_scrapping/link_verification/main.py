4211be35e16e667698866a969eadad479b1bc4c0
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2025-05-13 09:37:21 +0300

    Chore(C11): Explain Program Functionality
    The comments explain the purpose of the program and act as a development
    roadmap

---
+++ automating-tasks/chapter-11_web_scrapping/link_verification/main.py
@@ -0,0 +1,11 @@
+#! /usr/bin/python3
+
+# Link_verification - Flags broken links from a provided url to a webpage
+
+# TODO: Open webpage from the provided url
+
+# TODO: Get all link elements from the webpage
+
+# TODO: Visit all links from the webpage
+
+# TODO: Flag 404 Error pages as broken links
